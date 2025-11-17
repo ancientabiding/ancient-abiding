6f242d8467588db6d2020576a0a2b80d7899f0d0
Greco <51969359+ancientabiding@users.noreply.github.com>
2025-11-17 00:59:35 -0300

    feat: add website boilerplate

---
+++ apps/website/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const sans = Inter({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Greco",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${sans.variable}`}>{children}</body>
+    </html>
+  );
+}
